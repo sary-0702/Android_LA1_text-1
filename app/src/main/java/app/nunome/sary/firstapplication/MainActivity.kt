a51681e52db2adf00d9d96a7b8f907d70faa5593
sary-0702 <nya.saku.7722@gmail.com>
2021-01-31 21:50:16 +0900

    1

---
+++ app/src/main/java/app/nunome/sary/firstapplication/MainActivity.kt
@@ -0,0 +1,16 @@
+package app.nunome.sary.firstapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            button.text = "クリックされました！"
+        }
+    }
+}
